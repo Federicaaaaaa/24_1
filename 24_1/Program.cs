aff38c1d24c37ede9538b10c23747acfed385d12
Federicaaaaaa <danger2006.bona@gmail.com>
2023-11-28 22:15:46 +0100

    Aggiungere i file di progetto.

---
+++ 24_1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _24_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Quadrato q = new Quadrato();
+            q.Lato = float.Parse(Console.ReadLine());
+            Console.WriteLine(q.Lato*q.Lato);
+            Console.ReadLine();
+        }
+    }
+}
